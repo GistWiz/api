30b9477ce0a45444872867ad40ff3be84cb41e01
Wil (₩) Moore III <51953+wilmoore@users.noreply.github.com>
2024-12-04 00:08:45 -0700

    Productivity Dashboard as empty auto-complete term

--- plugin/raycast/src/lib/quicksearch.ts
+++ plugin/raycast/src/lib/quicksearch.ts
@@ -1,7 +1,7 @@
 import { URLSearchParams } from 'node:url'
 import { useMemo } from 'react'
 
-const generate = (host: string, query: string) => `http://${host}/qs?${new URLSearchParams({ query: query.length === 0 ? '0' : query })}`
+const generate = (host: string, query: string) => `http://${host}/qs?${new URLSearchParams({ query: query.length === 0 ? 'Productivity Dashboard' : query })}`
 
 export const useQuickSearch = ({ host, query }: { host: string, query: string}) => ({
   quickSearchUrl: useMemo(() => generate(host, query), [host, query])
